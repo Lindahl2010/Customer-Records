0ec546bafbd50aafc032b8e66759ab41ebbcfa3a
Eli Lindahl <lindahl2010@hotmail.com>
2018-02-07 08:02:47 -0600

    Initial commit of Customer Records program

--- Customer Records/Program.cs
+++ Customer Records/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Customer_Records
 {
@@ -10,6 +12,37 @@ namespace Customer_Records
     {
         static void Main(string[] args)
         {
+            string text;
+            ConsoleKeyInfo keyValue;
+
+            welcome(out keyValue);
+
+            while (keyValue.Key == ConsoleKey.Enter)
+            {
+
+
+                FileStream file = new FileStream("WriteCustomerRecords.txt", FileMode.Create, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(file);
+                WriteLine("Please Enter your data: ");
+                text = ReadLine();
+                writer.WriteLine(text);
+
+                writer.Close();
+                file.Close();
+            }
+
+        }
+
+        public static void welcome(out ConsoleKeyInfo keyValue)
+        {
+            WriteLine("Welcome to the Customer Records Program");
+            WriteLine("\nTo start the program, please press Enter or any key to exit...");
+            keyValue = ReadKey();
+        }
+        
+        public static void display()
+        {
+
         }
     }
 }
