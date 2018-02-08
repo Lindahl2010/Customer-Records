e02c372af9cdd7bb96d688335d4ee3f75b177caa
Lindahl <linelij@dunwoody.edu>
2018-02-07 19:35:01 -0600

    Keeping repo up to date

--- Customer Records/Program.cs
+++ Customer Records/Program.cs
@@ -12,23 +12,35 @@ namespace Customer_Records
     {
         static void Main(string[] args)
         {
-            string text;
             ConsoleKeyInfo keyValue;
+            Customer client = new Customer();
+            string userInput;
+            List<String> customerList = new List<String>();
 
             welcome(out keyValue);
 
             while (keyValue.Key == ConsoleKey.Enter)
             {
-
-
                 FileStream file = new FileStream("WriteCustomerRecords.txt", FileMode.Create, FileAccess.Write);
                 StreamWriter writer = new StreamWriter(file);
-                WriteLine("Please Enter your data: ");
-                text = ReadLine();
-                writer.WriteLine(text);
-
+                Write("ID Number: ");
+                client.idNumber = ReadLine();
+                Write("Name: ");
+                client.name = ReadLine();
+                Write("Balance: ");
+                client.balance = ReadLine();
+                WriteLine("ID Number: {0} Name: {1} Balance: {2}\n", client.idNumber, client.name, client.balance);
+                customerList.Add(client.idNumber);
+                customerList.Add(client.name);
+                customerList.Add(client.balance);
+                foreach (var i in customerList)
+                {
+                    writer.WriteLine($"{ i} ");
+                }
                 writer.Close();
                 file.Close();
+
+                restart(out keyValue);
             }
 
         }
@@ -39,10 +51,24 @@ namespace Customer_Records
             WriteLine("\nTo start the program, please press Enter or any key to exit...");
             keyValue = ReadKey();
         }
-        
-        public static void display()
-        {
 
+        public static void restart(out ConsoleKeyInfo keyValue)
+        {
+            WriteLine("To restart the program, please press Enter or any other key to exit.");
+            keyValue = ReadKey();
+
+            if (keyValue.Key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                welcome(out keyValue);
+            }
+        }
+        
+        class Customer
+        {
+            public string idNumber { get; set; }
+            public string name { get; set; }
+            public string balance { get; set; }          
         }
     }
 }
